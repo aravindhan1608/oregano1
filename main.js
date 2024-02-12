3684f8f37de628dcd7eb85df335b661461c979b7
aravindhan <aravindhan70@gmail.com>
2024-02-12 17:51:12 +0530

    cart inset

---
+++ main.js
@@ -0,0 +1,8 @@
+function incrementButton()
+{
+    var element =document.getElementById('cartincrement');
+    var value =element.innerHTML;
+    ++ value;
+    console.log(value);
+    document.getElementById('cartincrement').innerHTML=value;
+}
